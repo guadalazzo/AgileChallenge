91793258bdedaefca592e1b8c50102af0af7fb78
Guadalupe Jazmin Lazzo <guadalupelazzo@gmail.com>
2019-06-09 11:33:13 -0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import './App.css';
+import ControlPanel from "./control-panel/ControlPanel";
+import FileZone from "./file-zone/FileZone";
+import getMockText from './text.service';
+
+class App extends Component {
+    getText() {
+        getMockText().then(function (result) {
+            console.log(result);
+        });
+    }
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <span>Simple Text Editor</span>
+                </header>
+                <main>
+                    <ControlPanel/>
+                    <FileZone/>
+                </main>
+            </div>
+        );
+    }
+}
+
+export default App;
