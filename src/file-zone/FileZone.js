0546522c6f664b882ee526ec744fadb4a7070dcb
Guadalupe Jazmin Lazzo <guadalupelazzo@gmail.com>
2019-06-09 12:01:09 -0300

    The text is render

--- src/file-zone/FileZone.js
+++ src/file-zone/FileZone.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import getMockText from '../text.service';
 import './FileZone.css';
 
 class FileZone extends Component {
+    constructor(){
+        super();
+        this.state = {
+            results:'',
+        }
+    }
+    componentDidMount() {
+        getMockText()
+            .then(results => this.setState({results}))
+            .catch(err=> console.log('ERR: ',err))
+    }
     render() {
         return (
             <div id="file-zone">
                 <div id="file">
-
+                    <p>{ '' || this.state.results}</p>
                 </div>
             </div>
         );
