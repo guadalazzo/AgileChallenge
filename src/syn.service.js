fde22afb6d0beb046035fc758cb1ca989a89f3f2
Guadalupe Jazmin Lazzo <guadalupelazzo@gmail.com>
2019-06-09 22:14:57 -0300

    added limit

--- src/syn.service.js
+++ src/syn.service.js
@@ -1,6 +1,7 @@
 const URL = 'https://api.datamuse.com/words?ml=';
 const axios = require('axios');
+const limit = '10';
 
 export default function getSynonym(word) {
-  return axios.get(URL+word);
+  return axios.get(`${URL}${word}&max=${limit}`);
 }
