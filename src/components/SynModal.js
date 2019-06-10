8c6ad08c7c8dd39a02341c8ac364c4396320008d
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2019-06-09 23:17:17 -0300

    working on popup

--- src/components/SynModal.js
+++ src/components/SynModal.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './SynModal.css';
 
 const SynModal = (props) => {
   console.log(props);
-  return <React.Fragment>{props.synList.map(syn=> <p> {syn.word} </p> )}</React.Fragment>
+  return <div className="popup">{props.synList.map(syn=> <p> {syn.word} </p> )}</div>
   //  return <ul> {props.synList.map(syn => <li>{syn}</li>)}</ul>
 }
 export default SynModal;
