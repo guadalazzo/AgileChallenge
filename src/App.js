933595c9c9af7f79d19ae97f048604d2d4098d69
guadalazzo <guadalazzo@est.frba.utn.edu.ar>
2019-06-17 17:43:51 -0300

    added comments and empty synonym state

--- src/App.js
+++ src/App.js
@@ -32,13 +32,14 @@ class App extends Component {
                     text: word,
                     index,
                 }))
+                // If has no localStorage info, take the service results
                 localStorage.getItem('words') && JSON.parse(localStorage.getItem('words')).length > 0 ? 
                 this.setState({results: JSON.parse(localStorage.getItem('words'))}) : 
                 this.setState({results: wordsList});
             })
             .catch(err=> console.log('ERR on Initial render: ',err))
     }
-    getSyn(word) {
+    getSyn(word) { // It brings the array of synonyms 
          getSynonym(word)
         .then(res => this.setState({selectedWord:{...this.state.selectedWord, synList: res.data}}))
         .catch(err => console.log('ERR charging synonyms: ',err));
@@ -46,6 +47,7 @@ class App extends Component {
     
     handleClick(type) {
         const { text, index } = this.state.selectedWord;
+        // it handles the word style type, after that it change the results array with the new type
         if (this.state.selectedWord.index !== null) {
             if (type === 'bold') {
                 this.state.results[index].bold = !this.state.results[index].bold;
@@ -62,12 +64,15 @@ class App extends Component {
         }
     }
     handleDoubleClick(e, index) {
+        // brings the synonyms list to the state, also get the selected word
         this.setState({selectedWord:{index: index, text:e.target.textContent, synList: this.getSyn(e.target.textContent)}});
     }
     setLocalStorage(results){
+        // set de storage info
         localStorage.setItem('words', JSON.stringify(results));
     }
     changeWord(word) {
+        // change the word selected for the synonym
         this.state.results[this.state.selectedWord.index].text = word;
         this.setState({results: this.state.results})
         this.setLocalStorage(this.state.results)
@@ -79,7 +84,7 @@ class App extends Component {
                     <span>Simple Text Editor</span>
                 </header>
                 <main>
-                   {this.state.selectedWord.synList && this.state.selectedWord.synList.length > 0 && <SynModal synList={this.state.selectedWord.synList} onClick={this.changeWord}/>} 
+                   {this.state.selectedWord.synList && <SynModal synList={this.state.selectedWord.synList} onClick={this.changeWord}/>} 
                     <ControlPanel onClick={this.handleClick}/>
                     <FileZone getRef={this.getRef} results={this.state.results} onDoubleClick={this.handleDoubleClick}/>
                 </main>
--- src/components/SynModal.js
+++ src/components/SynModal.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './SynModal.css';
 
 const SynModal = (props) => {
-  return (
+  return props.synList.length > 0 ? (
   <div className="popup">
     <h3>Change it for a synonym:</h3>
-    {props.synList.map(syn=> <span onClick={() => props.onClick(syn.word)}> {syn.word} </span> )}
+    {props.synList.map(syn=> <span key={`a-${syn.word}`} onClick={() => props.onClick(syn.word)}> {syn.word} </span> )}
     
-  </div>);
+  </div>): (<div className="popup"><h4>No synonyms were found</h4></div>);
 }
 export default SynModal;
